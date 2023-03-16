e5a42aff21acf6650e75c427d14eac1a0f3be088
iustus <lindgreia@gmail.com>
2023-03-15 21:50:06 -0400

    `delete`

--- public/netlify/functions/delete-customers/delete-customers.js
+++ public/netlify/functions/delete-customers/delete-customers.js
@@ -1,17 +1,19 @@
 const mongoose = require('mongoose');
 
-// Create a connection pool
-const uri = 'mongodb+srv://root:Aym%40n1504@cluster0.0rfiy6k.mongodb.net/db';
-const options = { useNewUrlParser: true, poolSize: 10, useUnifiedTopology: true };
-const connectionPool = mongoose.createPool(uri, options);
-
-exports.handler = async (event, context) => {
-  const Customer = connectionPool.model('vlfsr', new mongoose.Schema({
+mongoose.connect('mongodb+srv://root:Aym%40n1504@cluster0.0rfiy6k.mongodb.net/db', {
+  useNewUrlParser: true
+}).then(() => console.log('MongoDB is on :D')).catch(err => console.log(err));
+  const Customer = mongoose.model('vlfsr', new mongoose.Schema({
     aptName: String,
     filterSize: String
   }));
 
+
+exports.handler = async (event, context) => {
+
+
   const { id } = event.queryStringParameters;
+  console.log(id);
   await Customer.findByIdAndDelete(id);
 
   return {
