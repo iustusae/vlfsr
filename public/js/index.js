8978406dd24291dbdd4ab2feecc07143c5761b4a
iustus <lindgreia@gmail.com>
2023-03-15 20:08:53 -0400

    `netlify functions`

--- public/js/index.js
+++ public/js/index.js
@@ -1,34 +1,26 @@
 const express = require('express');
+const serverless = require('serverless-http');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-app.use(bodyParser.json());
-app.use(cors());
-//app.use(express.static('src'));
-
-const port = process.env.PORT || 3003;
-mongoose.connect('mongodb+srv://root:Aym%40n1504@cluster0.0rfiy6k.mongodb.net/db', {
-    useNewUrlParser: true
-}).then(() => console.log('MongoDB is on :D')).catch(err => console.log(err));
 const CustomerSchema = new mongoose.Schema({
     aptName: String,
     filterSize: String
 });
 const Customer = mongoose.model('vlfsr', CustomerSchema);
+
+app.use(bodyParser.json());
+app.use(cors());
+
+mongoose.connect('mongodb+srv://root:Aym%40n1504@cluster0.0rfiy6k.mongodb.net/db', {
+    useNewUrlParser: true
+}).then(() => console.log('MongoDB is on :D')).catch(err => console.log(err));
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
-app.get('/cstm', async (req, res) => {
-    const Customers = await Customer.find();
-    res.json(Customers);
-});
-app.post('/cstm', async (req, res) => {
-    console.log(req.body);
-    const newCustomer = new Customer(req.body);
-    await newCustomer.save();
-    res.json(newCustomer);
-});
+
 app.put('/cstm/:id', async (req, res) => {
     const {
         id
@@ -38,6 +30,7 @@ app.put('/cstm/:id', async (req, res) => {
     });
     res.json(updatedCustomer);
 });
+
 app.delete('/cstm/:id', async (req, res) => {
     const {
         id
@@ -45,4 +38,5 @@ app.delete('/cstm/:id', async (req, res) => {
     await Customer.findByIdAndDelete(id);
     res.sendStatus(204);
 });
-app.listen(port, () => console.log(`Server running on port ${port}`));
+
+module.exports.handler = serverless(app);
