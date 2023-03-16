3efc81a7a6fb8a6b45eb439bdecca035f53b4de9
iustus <lindgreia@gmail.com>
2023-03-15 22:05:35 -0400

    AAAAAAAAAAAAAAA

--- public/netlify/functions/delete-customers/delete-customers.js
+++ public/netlify/functions/delete-customers/delete-customers.js
@@ -1,23 +1,32 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://root:Aym%40n1504@cluster0.0rfiy6k.mongodb.net/db', {
-  useNewUrlParser: true
-}).then(() => console.log('MongoDB is on :D')).catch(err => console.log(err));
 const Customer = mongoose.model('vlfsr', new mongoose.Schema({
   aptName: String,
   filterSize: String
 }));
 
-
 exports.handler = async (event, context) => {
+  try {
+    await mongoose.connect('mongodb+srv://root:Aym%40n1504@cluster0.0rfiy6k.mongodb.net/db', {
+      useNewUrlParser: true
+    });
+    console.log('MongoDB is on :D');
 
+    const { id } = event.queryStringParameters;
+    console.log(id);
+    await Customer.findByIdAndDelete(id);
 
-  const { id } = event.queryStringParameters;
-  console.log(id);
-  await Customer.findByIdAndDelete(id);
+    await mongoose.connection.close();
 
-  return {
-    statusCode: 204,
-    body: ''
-  };
+    return {
+      statusCode: 204,
+      body: ''
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: 'Internal Server Error'
+    };
+  }
 };
